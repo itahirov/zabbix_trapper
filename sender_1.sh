c800fd43d2cafc09f8000add4a8abe249a16838b
itahirov <38325195+itahirov@users.noreply.github.com>
2022-01-04 21:02:17 -0500

    Update sender_1.sh

--- sender_1.sh
+++ sender_1.sh
@@ -2,3 +2,4 @@
 find /tmp -type f -atime +10 -delete
 statuscode=$?
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "$(hostname)" -k temp.cleanup -o $statuscode
+# k	The item key that the data will be recorded against. In this case we will make an item called temp.cleanup in Zabbix and the script will record its data there.
